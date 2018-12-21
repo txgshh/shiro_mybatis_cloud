503bba88ccdbd7b67c0a5c01ab0e4ec501bb032d
txgshh <36064800+txgshh@users.noreply.github.com>
2018-12-21 21:11:49 +0800

    正常登陆
    正常登陆

--- gas-cloud-user/src/main/java/com/xgjgas/gascloud/com/yphx/controller/SystemController.java
+++ gas-cloud-user/src/main/java/com/xgjgas/gascloud/com/yphx/controller/SystemController.java
@@ -7,22 +7,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.xgjgas.gascloud.com.yphx.domain.XgjSysUser;
+import com.xgjgas.gascloud.com.yphx.domain.XgjSysUserExample;
+import com.xgjgas.gascloud.com.yphx.service.IXgjSysUserService;
+import com.xgjgas.gascloud.com.yphx.shiro.realm.GasAuthorizingRealm.GasPrincipal;
 
 @Controller
 public class SystemController {
+	@Autowired
+	private IXgjSysUserService xgjSysUserService;
+	
 	@RequestMapping("/login")
 	public String loginGet() {
 		return "login";
 	}
 	
 	@RequestMapping(value="/index",method=RequestMethod.GET)
-	@RequiresPermissions(value={"gas:selectCity:view1"},logical=Logical.OR)
+	//@RequiresPermissions(value={"gas:selectCity:view1"},logical=Logical.OR)
 	public String loginPost() {
-		/*XgjSysUser user = SysAuthorityUtil.getSysUser();
+		Subject sub = SecurityUtils.getSubject();
+		GasPrincipal principal = (GasPrincipal)sub.getPrincipal();
+
+    		XgjSysUserExample xgjSysUserCriteria = new XgjSysUserExample();
+		XgjSysUserExample.Criteria criteria = xgjSysUserCriteria.createCriteria();
+		criteria.andIdEqualTo(principal.getId());
+		XgjSysUser user = xgjSysUserService.getObject(principal.getId());
 		// 如果已经登录，则跳转到管理首页
-		if (user.getLoginName() != null) {
+		if (user!=null&&user.getLoginName() != null) {
 			return "redirect:/";
-		}*/
+		}
 		return "/index";
 	}
 }
